9ceb2060f3de603cd306a5ed1d6ae5e9c45adc73
Anfinonty <alienboi2726@gmail.com>
2023-12-26 21:24:45 +0800

    12-26-2023 no memleaks for rotation, sprite is not drawn for now

--- gr.c
+++ gr.c
@@ -122,29 +122,109 @@ void GrLine(HWND hwnd, HDC hdc, PAINTSTRUCT ps,double x1,double y1,double x2,dou
 }
 
 
-void GrSprite(HWND hwnd, HDC hdc, PAINTSTRUCT ps,  double x1, double y1, HBITMAP src_Bitmap, bool is_left) {
-  BITMAP bitmap;
-  HBITMAP oldBitmap;
-  HDC hdcMem;
+void GrSprite(HWND hwnd, HDC hDC, PAINTSTRUCT ps,  double _x1, double _y1, HBITMAP hSourceBitmap, bool is_left) {
+   HBITMAP hOldSourceBitmap, hOldDestBitmap, hDestBitmap;
+   HDC hMemSrc,hMemDest;
+   BITMAP iSrcBitmap;
 
-  if (src_Bitmap!=NULL) {
-    src_Bitmap=Rotate(hdc, src_Bitmap, le_angle);
+   // Step 1: Create a memory DC for the source and destination bitmaps
+   //         compatible with the device used.
+   hMemSrc = CreateCompatibleDC(hDC);
+   hMemDest= CreateCompatibleDC(hDC);
 
-    hdcMem=CreateCompatibleDC(hdc);
-    oldBitmap = SelectObject(hdcMem, src_Bitmap);
-    GetObject(src_Bitmap, sizeof(bitmap), &bitmap); //hBitmap point to bitmap var
-    //BitBlt(hdc, x1-bitmap.bmWidth/2, y1-bitmap.bmHeight, bitmap.bmWidth, bitmap.bmHeight, hdcMem, 0, 0, SRCAND);
+  // Step 2: Get the height and width of the source bitmap.
+   GetObject(hSourceBitmap, sizeof(iSrcBitmap), (LPSTR)&iSrcBitmap);
 
-    if (is_left) { //Flip Horizontally (X)
-      StretchBlt(hdcMem, x1+bitmap.bmWidth/2, y1-bitmap.bmHeight, -bitmap.bmWidth-1, bitmap.bmHeight, hdc, 0,0, bitmap.bmWidth, bitmap.bmHeight, SRCCOPY);
-      StretchBlt(hdc, x1+bitmap.bmWidth/2, y1-bitmap.bmHeight, -bitmap.bmWidth-1, bitmap.bmHeight, hdcMem, 0,0, bitmap.bmWidth, bitmap.bmHeight, SRCAND);
-    } else { //Regular
-      StretchBlt(hdcMem, x1-bitmap.bmWidth/2, y1-bitmap.bmHeight, bitmap.bmWidth, bitmap.bmHeight, hdc, 0,0, bitmap.bmWidth, bitmap.bmHeight, SRCCOPY);
-      StretchBlt(hdc, x1-bitmap.bmWidth/2, y1-bitmap.bmHeight, bitmap.bmWidth, bitmap.bmHeight, hdcMem, 0,0, bitmap.bmWidth, bitmap.bmHeight, SRCAND);
-    }
-    SelectObject(hdcMem, oldBitmap);
-    DeleteDC(hdcMem);
-  }
+// Get logical coordinates
+   double cosine = (double)cos(le_angle);
+   double sine = (double)sin(le_angle);
+
+	// Compute dimensions of the resulting bitmap
+	// First get the coordinates of the 3 corners other than origin
+   int x1 = (int)(-iSrcBitmap.bmHeight * sine);
+   int y1 = (int)(iSrcBitmap.bmHeight * cosine);
+   int x2 = (int)(iSrcBitmap.bmWidth * cosine - iSrcBitmap.bmHeight * sine);
+   int y2 = (int)(iSrcBitmap.bmHeight * cosine + iSrcBitmap.bmWidth * sine);
+   int x3 = (int)(iSrcBitmap.bmWidth * cosine);
+   int y3 = (int)(iSrcBitmap.bmWidth * sine);
+
+   int minx = min(0,min(x1, min(x2,x3)));
+   int miny = min(0,min(y1, min(y2,y3)));
+   int maxx = max(0,max(x1, max(x2,x3)));
+   int maxy = max(0, max(y1, max(y2,y3)));
+
+   int width = maxx - minx;
+   int height = maxy - miny;
+
+   // Step 3: Select the source bitmap into the source DC. Create a
+   //         destination bitmap, and select it into the destination DC.
+
+   hDestBitmap = NULL;//CreateCompatibleBitmap(hMemDest, width, height);
+   //hDestBitmap = CreateCompatibleBitmap(hDC, width, height);
+
+
+   hOldSourceBitmap = SelectObject(hMemSrc, hSourceBitmap);
+   hOldDestBitmap = SelectObject(hMemDest, hDestBitmap);
+
+
+	// Draw the background color before we change mapping mode
+    COLORREF clrBack = RGB(255,255,255); //For transparent background
+	HBRUSH hbrBack = CreateSolidBrush( clrBack );
+	HBRUSH hbrOld = (HBRUSH) SelectObject( hMemDest, hbrBack );
+	PatBlt(hMemDest, 0, 0, width, height, PATCOPY );
+	DeleteObject( SelectObject(hMemDest, hbrOld ) );
+
+	// Set mapping mode so that +ve y axis is upwords
+	SetMapMode(hMemSrc, MM_ISOTROPIC);
+	SetWindowExtEx(hMemSrc, 1,1,NULL);
+	SetViewportExtEx(hMemSrc, 1,-1,NULL);
+	SetViewportOrgEx(hMemSrc, 0, iSrcBitmap.bmHeight-1,NULL);
+
+	SetMapMode(hMemDest, MM_ISOTROPIC);
+	SetWindowExtEx(hMemDest, 1,1,NULL);
+	SetViewportExtEx(hMemDest, 1,-1,NULL);
+	SetWindowOrgEx(hMemDest, minx, maxy,NULL);
+
+
+   // Step 4: Copy the pixels from the source to the destination.
+   for (int y=miny;y<maxy;y++) {
+	 for(int x=minx;x<maxx;x++) {
+	   int sourcex = (int)(x*cosine+y*sine);
+	   int sourcey = (int)(y*cosine-x*sine);
+	   if(sourcex>=0 && sourcex<iSrcBitmap.bmWidth && sourcey>=0
+	   	   && sourcey<iSrcBitmap.bmHeight )
+	     SetPixel(hMemDest, x, y, GetPixel(hMemSrc,sourcex,sourcey));
+	  }
+	}
+
+
+  // GetObject(hDC, sizeof(iSrcBitmap), &iSrcBitmap); //hBitmap point to bitmap var
+   // Step 5: Destroy the DCs.
+   SelectObject(hMemSrc, hOldSourceBitmap);
+   SelectObject(hMemDest, hOldDestBitmap);
+   DeleteDC(hMemDest);
+   DeleteDC(hMemSrc);
+
+/*    src_Bitmap=Rotate(hdc, src_Bitmap, le_angle);*/
+   HBITMAP oldBitmap;
+   HDC hdcMem;
+
+   hdcMem=CreateCompatibleDC(hDC);
+   oldBitmap = SelectObject(hdcMem, hDestBitmap);
+   GetObject(hDestBitmap, sizeof(iSrcBitmap), &iSrcBitmap); //hBitmap point to bitmap var
+
+   if (is_left) { //Flip Horizontally (X)
+     StretchBlt(hdcMem, _x1+iSrcBitmap.bmWidth/2, _y1-iSrcBitmap.bmHeight, -iSrcBitmap.bmWidth-1, iSrcBitmap.bmHeight, hDC, 0,0, iSrcBitmap.bmWidth, iSrcBitmap.bmHeight, SRCCOPY);
+     StretchBlt(hDC, _x1+iSrcBitmap.bmWidth/2, _y1-iSrcBitmap.bmHeight, -iSrcBitmap.bmWidth-1, iSrcBitmap.bmHeight, hdcMem, 0,0, iSrcBitmap.bmWidth, iSrcBitmap.bmHeight, SRCAND);
+   } else { //Regular
+     StretchBlt(hdcMem, _x1-iSrcBitmap.bmWidth/2, _y1-iSrcBitmap.bmHeight, iSrcBitmap.bmWidth, iSrcBitmap.bmHeight, hDC, 0,0, iSrcBitmap.bmWidth, iSrcBitmap.bmHeight, SRCCOPY);
+     StretchBlt(hDC, _x1-iSrcBitmap.bmWidth/2, _y1-iSrcBitmap.bmHeight, iSrcBitmap.bmWidth, iSrcBitmap.bmHeight, hdcMem, 0,0, iSrcBitmap.bmWidth, iSrcBitmap.bmHeight, SRCAND);
+   }
+
+   //BitBlt(hDC, _x1-iSrcBitmap.bmWidth/2, _y1-iSrcBitmap.bmHeight, iSrcBitmap.bmWidth, iSrcBitmap.bmHeight, hdcMem, 0, 0, SRCAND);
+   SelectObject(hdcMem, oldBitmap);
+   DeleteDC(hdcMem);
+  //}
 }
 
 
