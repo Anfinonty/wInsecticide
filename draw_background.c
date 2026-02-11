f42e925e66cbbc7d998341d0ab98e5d8f26db2cc
Anfinonty <97777854+Anfinonty@users.noreply.github.com>
2026-02-12 00:45:18 +0800

    2026-02-12 no crash with this

--- draw_background.c
+++ draw_background.c
@@ -1184,8 +1184,8 @@ void DrawBackground(HDC hdc,HDC hdc2)
   int draw_p_py=(int)parralax_y;
 
   int dbg_height=GR_HEIGHT;
-  if (GR_HEIGHT>SCREEN_HEIGHT-1);
-    dbg_height=SCREEN_HEIGHT-1;
+  if (GR_HEIGHT>SCREEN_HEIGHT);
+    dbg_height=SCREEN_HEIGHT;
   switch (map_background) {
     case 0: //day and night
       if (flag_draw_game_background_spriteII) {
